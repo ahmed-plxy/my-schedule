7b38db6b8f53bd49d11bde25be441fc4988d2d6c
Ahmed Mahmoud <ahmedplxyman@gmail.com>
2026-04-29 00:00:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,13 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('app-cache').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js'
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
       ]);
     })
   );
@@ -13,6 +16,6 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
